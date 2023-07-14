0c800b7469d77c1c6d7f3b3b487ee40e2161d7d0
nykiway <nicoleoliviawiehe@gmail.com>
2023-07-13 19:01:30 -0700

    Initial commit

---
+++ config/config.exs
@@ -0,0 +1,32 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :users_api,
+  ecto_repos: [UsersApi.Repo],
+  generators: [binary_id: true]
+
+# Configures the endpoint
+config :users_api, UsersApiWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "vZReeXVcd8UankhTPwxj66Drof0DjGg+h8N4G79yXu4piHuxf6mKElZtDLzIqahM",
+  render_errors: [view: UsersApiWeb.ErrorView, accepts: ~w(json), layout: false],
+  pubsub_server: UsersApi.PubSub,
+  live_view: [signing_salt: "rQZ9KGZ8"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
